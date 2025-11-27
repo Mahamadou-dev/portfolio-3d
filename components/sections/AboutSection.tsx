f8ef946af7f148867f8afe77f6daa68ea386370f
Mahamadou Amadou Habou Gremah <mahamadou8877@gmail.com>
2025-11-27 08:07:54 +0100

    setting the CV link on about section

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -346,7 +346,7 @@ export default function AboutSection() {
                 className="px-6 py-3 font-medium rounded-lg shadow-lg transition-all duration-300 relative overflow-hidden group bg-gradient-to-r from-blue-600 to-purple-600 text-white flex items-center justify-center text-sm"
                 whileHover={{ scale: 1.05, boxShadow: "0 10px 20px rgba(102, 126, 234, 0.3)" }}
                 whileTap={{ scale: 0.95 }}
-              ><a href="https://drive.google.com/file/d/1a1LaOMFQNOjjySmAWbablHvQXDWL6XMy/view?usp=sharing" target='_blank'>
+              ><a href="https://flowcv.com/resume/a5spl1e2vu5a" target='_blank'>
                 <span className="relative z-10 flex items-center justify-center">
                   <Download className="w-4 h-4 mr-2" />
                   {t("about.download_my_resume_button_text")}
